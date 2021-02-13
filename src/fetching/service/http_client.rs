ee67c2c3bbb555cd9e231dd4af5c8a81e052a811
Mikhail Svetov <svmk-tmsk@yandex.ru>
2021-02-13 21:48:10 +0700

    Fixes.

--- src/fetching/service/http_client.rs
+++ src/fetching/service/http_client.rs
@@ -67,6 +67,7 @@ impl HttpClient {
             },
         };
         let request_builder = self.client.request(request_method, request.get_url().clone());
+        let request_builder = request_builder.headers(request.get_headers().clone());
         let response = request_builder.send().await?;
         if request.get_check_status_code() {
             if response.status() != reqwest::StatusCode::OK {
--- src/fetching/service/http_client/request.rs
+++ src/fetching/service/http_client/request.rs
@@ -1,6 +1,9 @@
+use crate::prelude::*;
 use crate::fetching::model::url::Url;
 use crate::fetching::model::mime_type::MimeType;
 use crate::fetching::service::http_client::request_method::RequestMethod;
+use reqwest::header::HeaderMap;
+use reqwest::header::HeaderValue;
 
 #[derive(Debug)]
 pub struct Request {
@@ -8,6 +11,7 @@ pub struct Request {
     request_method: RequestMethod,
     check_status_code: bool,
     expected_mimes: Vec<MimeType>,
+    headers: HeaderMap,
 }
 
 impl Request {
@@ -17,6 +21,7 @@ impl Request {
             request_method: RequestMethod::Get,
             check_status_code: true,
             expected_mimes: Vec::new(),
+            headers: HeaderMap::new(),
         };
     }
 
@@ -26,16 +31,23 @@ impl Request {
             request_method: RequestMethod::Post,
             check_status_code: true,
             expected_mimes: Vec::new(),
+            headers: HeaderMap::new(),
         };
-    }
-
-    pub fn get_method(&self) -> &RequestMethod {
-        return &self.request_method;
     }
 
     pub fn with_mime_type(mut self, mime_type: MimeType) -> Self {
         self.expected_mimes = vec![mime_type];
         return self;
+    }
+
+    pub fn with_header(mut self, key: &'static str, value: &str) -> Result<Self, Failure> {
+        let value = HeaderValue::from_str(value)?;
+        let _ = self.headers.insert(key, value);
+        return Ok(self);
+    }
+
+    pub fn get_method(&self) -> &RequestMethod {
+        return &self.request_method;
     }
 
     pub fn get_url(&self) -> &Url {
@@ -48,5 +60,9 @@ impl Request {
 
     pub fn get_expected_mimes(&self) -> &Vec<MimeType> {
         return &self.expected_mimes;;
+    }
+
+    pub fn get_headers(&self) -> &HeaderMap {
+        return &self.headers;
     }
 }
