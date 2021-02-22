563baeef8d34bf60bda3e5b8f23a35d5374ff094
Mikhail Svetov <svmk-tmsk@yandex.ru>
2021-02-23 06:20:06 +0700

    Fixes.

--- src/market/fund_report/model/fund_changes_id.rs
+++ src/market/fund_report/model/fund_changes_id.rs
@@ -9,6 +9,16 @@ pub struct FundChangesId {
     next_fund_id: DailyFundReportId,
 }
 
+impl FundChangesId {
+    pub fn get_prev_fund_id(&self) -> &DailyFundReportId {
+        return &self.prev_fund_id;
+    }
+    
+    pub fn get_next_fund_id(&self) -> &DailyFundReportId {
+        return &self.next_fund_id;
+    }
+}
+
 impl Identity for FundChangesId {
     fn to_string(&self) -> String {
         return format!("{}-{}", self.prev_fund_id.to_string(), self.next_fund_id.to_string());
--- src/market/fund_report/service/fund_changes_generator.rs
+++ src/market/fund_report/service/fund_changes_generator.rs
@@ -1,12 +1,30 @@
-use crate::market::fund_report::service::daily_fund_report_importing::DailyFundReportImporting;
+use crate::prelude::*;
 use crate::market::fund_report::model::fund_id::FundId;
 use crate::repository::repository::repository_instance::RepositoryInstance;
+use crate::market::fund_report::model::daily_fund_report::DailyFundReport;
+use crate::market::fund_report::model::daily_fund_report_id::DailyFundReportId;
+use crate::market::fund_report::model::fund_changes::FundChanges;
+use crate::market::fund_report::model::fund_changes_id::FundChangesId;
 use typed_di::service::Service;
 
 pub struct FundChangesGenerator {
-    daily_fund_report_importing: Service<DailyFundReportImporting>,
+    report_repository: Service<RepositoryInstance<DailyFundReportId, DailyFundReport>>,
+    fund_changes_repository: Service<RepositoryInstance<FundChangesId, FundChanges>>,
 }
 
 impl FundChangesGenerator {
-    
+    pub async fn generate_fund_changes(&self, fund_changes_id: FundChangesId) -> Result<FundChanges, Failure> {
+        let fund_changes = self.fund_changes_repository.find(&fund_changes_id).await?;
+        let fund_changes = match fund_changes {
+            Some(fund_changes) => fund_changes,
+            None => {
+                let prev_report = self.report_repository.get(fund_changes_id.get_prev_fund_id()).await?;
+                let next_report = self.report_repository.get(fund_changes_id.get_next_fund_id()).await?;
+                let fund_changes = FundChanges::generate(&prev_report, &next_report)?;
+                self.fund_changes_repository.store(&fund_changes).await?;
+                fund_changes
+            },
+        };
+        return Ok(fund_changes);
+    }   
 }
