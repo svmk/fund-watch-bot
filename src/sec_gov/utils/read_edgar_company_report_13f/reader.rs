a2f638aa9a0ca1607a8933f1c9fed01317d6ff35
Mikhail Svetov <svmk-tmsk@yandex.ru>
2021-03-06 15:37:26 +0700

    Fixes.

--- src/sec_gov/utils/read_edgar_company_report_13f/reader.rs
+++ src/sec_gov/utils/read_edgar_company_report_13f/reader.rs
@@ -10,13 +10,21 @@ use async_std::io::Lines;
 pub struct Reader {
     path: PathBuf,
     file: Lines<BufReader<File>>,
+    working_document: Option<EdgarDocument>,
+}
+
+#[derive(Debug)]
+enum LineContent<'a> {
+    Start(&'a str),
+    Content(&'a str),
+    End(&'a str),
+    None,
 }
 
 impl Reader {
-    const SEC_HEADER_END_TAG: &'static str = "</SEC-HEADER>";
-    const IMS_HEADER_END_TAG: &'static str = "</IMS-HEADER>";
-    const DOCUMENT_START_TAG: &'static str = "<DOCUMENT>";
-    const DOCUMENT_END_TAG: &'static str = "</DOCUMENT>";
+    // const SEC_HEADER_END_TAG: &'static str = "</SEC-HEADER>";
+    // const IMS_HEADER_END_TAG: &'static str = "</IMS-HEADER>";
+    const DOCUMENT_TAG: &'static str = "DOCUMENT";
 
     pub fn new(path: PathBuf, file: File) -> Reader {
         let file = BufReader::new(file);
@@ -24,50 +32,106 @@ impl Reader {
         return Reader {
             path,
             file,
+            working_document: None,
         };
     }
 
-    pub async fn skip_header(&mut self) -> Result<(), Failure> {
-        let mut header_was_skipped = false;
-        while let Some(line) = self.file.next().await {
-            let line = line?;
-            let line = line.trim();
-            if line == Self::SEC_HEADER_END_TAG {
-                header_was_skipped = true;
-                break;
-            }
-            if line == Self::IMS_HEADER_END_TAG {
-                header_was_skipped = true;
-                break;
-            }
+    // pub async fn skip_header(&mut self) -> Result<(), Failure> {
+    //     let mut header_was_skipped = false;
+    //     while let Some(line) = self.file.next().await {
+    //         let line = line?;
+    //         let line = line.trim();
+    //         if line == Self::SEC_HEADER_END_TAG {
+    //             header_was_skipped = true;
+    //             break;
+    //         }
+    //         if line == Self::IMS_HEADER_END_TAG {
+    //             header_was_skipped = true;
+    //             break;
+    //         }
+    //     }
+    //     if !header_was_skipped {
+    //         return crate::fail!("Edgar header not found at file `{}`", self.path.display());
+    //     }
+    //     return Ok(());
+    // }
+
+    fn get_content_tag<'a>(line: &'a str, tag: &str) -> LineContent<'a> {
+        let start_tag = format!("<{}>", tag);
+        let start_index = line
+            .find(&start_tag)
+            .map(|start_index| {
+                return start_index + start_tag.len();
+            });
+        let end_tag = format!("</{}>", tag);
+        let end_index = line.find(&end_tag);
+        match (start_index, end_index) {
+            (Some(start_index), Some(end_index)) => {
+                let line = &line[start_index..end_index];
+                return LineContent::Content(line);
+            },
+            (Some(start_index), None) => {
+                let line = &line[start_index..];
+                return LineContent::Start(line);
+            },
+            (None, Some(end_index)) => {
+                let line = &line[..end_index];
+                return LineContent::End(line);
+            },
+            (None, None) => {
+                return LineContent::None;
+            },
         }
-        if !header_was_skipped {
-            return Err(Failure::msg(format!("Edgar header not found at file `{}`", self.path.display())));
-        }
-        return Ok(());
     }
 
     pub async fn read_document(&mut self) -> Result<Option<EdgarDocument>, Failure> {
-        let mut working_document: Option<EdgarDocument> = None;
         while let Some(line) = self.file.next().await {
             let line = line?;
-            let line = line.trim();
-            if line == Self::DOCUMENT_START_TAG {
-                working_document = Some(EdgarDocument::new());
-            } else if line == Self::DOCUMENT_END_TAG {
-                if working_document.is_none() {
-                    return Err(Failure::msg(format!("Edgar document double end tag detected`{}`", self.path.display())));
-                }
-                return Ok(working_document);
-            } else {
-                if let Some(ref mut working_document) = working_document {
-                    working_document.push_line(line);
-                }
+            let line_content = Self::get_content_tag(&line, Self::DOCUMENT_TAG);
+            match line_content {
+                LineContent::Start(start_content) => {
+                    if self.working_document.is_some() {
+                        return crate::fail!("Edgar document double start tag detected `{}`", self.path.display());
+                    }
+                    self.working_document = Some(EdgarDocument::new());
+                    self.write_line_to_document(start_content, true)?;
+                },
+                LineContent::Content(content) => {
+                    if self.working_document.is_some() {
+                        return crate::fail!("Edgar document double start tag detected within content `{}`", self.path.display());
+                    }
+                    self.working_document = Some(EdgarDocument::new());
+                    self.write_line_to_document(content, true)?;
+                    return Ok(self.working_document.take());
+                },
+                LineContent::End(end_content) => {
+                    self.write_line_to_document(end_content, true)?;
+                    if self.working_document.is_none() {
+                        return crate::fail!("Edgar document double end tag detected `{}`", self.path.display());
+                    }
+                    return Ok(self.working_document.take());
+                },
+                LineContent::None => {
+                    if self.working_document.is_some() {
+                        self.write_line_to_document(&line, false)?;
+                    }
+                },
             }
         }
-        if working_document.is_some() {
-            return Err(Failure::msg(format!("Edgar document parsing incomplete `{}`", self.path.display())));
+        if self.working_document.is_some() {
+            return crate::fail!("Edgar document parsing incomplete `{}`", self.path.display());
         }
         return Ok(None);
+    }
+
+    fn write_line_to_document(&mut self, line: &str, skip_empty: bool) -> Result<(), Failure> {
+        if let Some(working_document) = self.working_document.as_mut() {
+            if skip_empty && line.is_empty() {
+                return Ok(());
+            }
+            working_document.push_line(line);
+            return Ok(());
+        }
+        return crate::fail!("Unable to write to edgar document: `{}`", self.path.display());
     }
 }
